99891cb663c1580e975cdd2ada6e5e13cf2d38bb
Shin <now.nsw@gmail.com>
2022-04-19 16:47:45 +0700

    Change recordSchema type: to string

--- src/models/record.js
+++ src/models/record.js
@@ -5,11 +5,15 @@ const recordSchema = new mongoose.Schema({
     type: String,
     minlength: [3, "Activity name should contains at least 3 chars"],
   },
-  date: { type: Date },
-  type: { type: String },
-  duration: { type: Number, min: [0, "Duration must be at least 0"] },
-  description: { type: String },
-  timestamp: { type: Date },
+  date: { type: String, require: true },
+  type: { type: String, require: true },
+  duration: {
+    type: String,
+    min: [0, "Duration must be at least 0"],
+    require: true,
+  },
+  description: { type: String, require: true },
+  timestamp: { type: String, require: true },
 });
 
 const RecordModel = mongoose.model("Record", recordSchema, "records");
